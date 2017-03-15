03f8d53fc5f5ae80f5f01ee1460f68f1cdc669d6
Nitehawk <nitehawk@nitespace.co>
2017-03-15 13:53:54 +0000

    Start building out command line script

---
+++ script/init.js
@@ -0,0 +1,4 @@
+// Download requested Minecraft server version and accept EULA
+var http = require('http');
+var fs = require('fs');
+
--- script/sts.js
+++ script/sts.js
@@ -1,18 +1,18 @@
+#!/usr/bin/env node
+"use strict";
 // Command line handler - sts.js
+
+const init = require('../script/init.js');
+
+// Get our current path so we can get back there if we move around
+let pwd = process.cwd();
 
 // Get our actual arguments
 var myArgs = process.argv.slice(2);
 
-switch (myArgs[0]) {
-  case 'configure':
-  case 'config':
-  case 'conf':
-    console.log("Configuring STS");
-    break;
-  case 'init':
-  case 'initialize':
-    console.log("Initializing directories..");
-    break;
-  default:
-    console.log("Err... not sure what you are trying to do there...");
-}
+// TODO: things to process:
+// Configure - create 'live' config files from our templates
+// Init - We should automatically do this step
+//  Grabs MC Server jar and creates EULA.txt
+// Pregen - Creates a number of worlds to use during a stream (optional)
+// Run - Runs an STS session
